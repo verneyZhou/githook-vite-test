b18b0276d24130a2982ec3fb8fb939507173764e
zhouyuan <zhouyuan10@staff.weibo.com>
2023-09-20 00:31:40 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 
 const env = process.env.ENV || ''; // 环境
+const OUTDIR = process.env.OUTDIR || 'dist'; // 输出目录
 
 // https://vitejs.dev/config/
 export default ({command, mode}) => {
@@ -11,5 +12,8 @@ export default ({command, mode}) => {
   return defineConfig({
     base: './', // 基础路径
     plugins: [vue()],
+    build: {
+      outDir: OUTDIR,
+    }
   })
 }
