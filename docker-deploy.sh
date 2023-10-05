fec67abb9af3d0d99597106f6b59216d961d540f
zhouyuan <zhouyuan10@staff.weibo.com>
2023-10-05 17:44:53 +0800

    fix: docker部署10

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,9 +1,10 @@
 
-#deploy.sh
+#!/usr/bin/env sh
 
-docker pull verneyzhou/githook-vite-test  # 拉取镜像
-docker stop githook-test-container  # 停止容器
-docker rm githook-test-container  # 删除容器
+docker login -u verneyzhou -p 20230725zy # 登录docker
+docker pull verneyzhou/githook-vite-test  # 拉取镜像, 如果没有指定tag就会默认是latest
+docker stop githook-aliyun-container  # 停止容器
+docker rm githook-aliyun-container  # 删除容器
 docker run -d -p 8397:80 --name githook-aliyun-container verneyzhou/githook-vite-test  # 运行容器
 
 # 清除一下没有使用的容器和镜像
