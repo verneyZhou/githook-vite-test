dc3a8ee279b098519ee53947d71b11aee38faa93
zhouyuan <zhouyuan17@baidu.com>
2024-09-23 15:04:36 +0800

    feat: 阿里云docker

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,22 +1,48 @@
 
 #!/usr/bin/env sh
 
-echo -e "---------docker Login--------"
-docker login -u $1  -p $2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
+# echo -e "---------docker Login--------"
+# docker login -u $1  -p $2 # 这个是yml文件最后一行所传递的参数，你docker的用户名和密码
+
+# echo -e "---------docker Pull--------"
+# docker pull verneyzhou/githook-vite-test  # 拉取镜像, 如果没有指定tag就会默认是latest
+
+# # 如果已经有同名的容器，删除掉
+# if [ "$(docker ps -aq -f name=githook-aliyun-container)" ]; then
+#   echo 'docker rm ...'
+#   docker stop githook-aliyun-container  # 停止容器
+#   docker rm githook-aliyun-container  # 删除容器
+# fi
+
+
+# echo -e "---------docker Create and Start--------"
+# docker run -d -p 8397:80 --name githook-aliyun-container verneyzhou/githook-vite-test:latest  # 运行容器
+
+# echo -e "---------deploy Clear--------"
+# # 清除一下没有使用的容器和镜像
+# docker image prune -f
+# docker container prune -f
+
+# echo -e "---------deploy Success---!!!!-----"
+
+
+###### docker 部署脚本 v2 ######
+echo -e "---------docker Login Aliyun--------"
+docker login --username=$1 registry.cn-beijing.aliyuncs.com -p $2
 
 echo -e "---------docker Pull--------"
-docker pull verneyzhou/githook-vite-test  # 拉取镜像, 如果没有指定tag就会默认是latest
+docker pull registry.cn-beijing.aliyuncs.com/zhou_aliyun_image/githook-vite-test:v1.0
 
 # 如果已经有同名的容器，删除掉
-if [ "$(docker ps -aq -f name=githook-aliyun-container)" ]; then
+if [ "$(docker ps -aq -f name=githook-aliyun-container-v2)" ]; then
   echo 'docker rm ...'
-  docker stop githook-aliyun-container  # 停止容器
-  docker rm githook-aliyun-container  # 删除容器
+  docker stop githook-aliyun-container-v2  # 停止容器
+  docker rm githook-aliyun-container-v2  # 删除容器
 fi
 
 
 echo -e "---------docker Create and Start--------"
-docker run -d -p 8397:80 --name githook-aliyun-container verneyzhou/githook-vite-test:latest  # 运行容器
+docker run -d -p 8396:80 --name githook-aliyun-container-v2 registry.cn-beijing.aliyuncs.com/zhou_aliyun_image/githook-vite-test:v1.0  # 运行容器
 
 echo -e "---------deploy Clear--------"
 # 清除一下没有使用的容器和镜像
